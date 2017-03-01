d3cf0d79f5dcbb5dcab6091dc31839f047732faa
Sofia Starikova <t4stso01@students.oamk.fi>
2017-03-01 14:59:52 +0200

    //Phase2

--- Data/data.js
+++ Data/data.js
@@ -11,7 +11,7 @@ var posts = [
             likes: 892,
 			userLike: false,
             caption: "pigs!",
-            tags: ['elections'],
+            tag: "pigs",
             comments: [{
                         id: 0,
                         user: {
@@ -21,38 +21,38 @@ var posts = [
                         },
                         text: "hahahaha",
                         userRefs: [],
-                        tags: []
+                        tag: "hahah"
                     }
             ]
 
         },
 				{
-								id: 0,
-								user: {
-										id: 1,
-										username: "harryp",
-										profileImageSmall: "http://bywilma.com/wp-content/uploads/2012/03/Cat-John.jpg"
-								},
-								image: "https://s-media-cache-ak0.pinimg.com/originals/5f/4e/b6/5f4eb67439ab51ed271c7b06c5c8b4ef.jpg",
-								imageThumbnail: "http://bywilma.com/wp-content/uploads/2012/03/Cat-John.jpg",
-								likes: 892,
+		            id: 1,
+		            user: {
+		                id: 2,
+		                username: "superuser",
+		                profileImageSmall: ""
+		            },
+		            image: "http://images.fastcompany.com/upload/adidas-jabulani-ball.jpg",
+		            imageThumbnail: "https://images-na.ssl-images-amazon.com/images/G/01/img15/pet-products/small-tiles/23695_pets_vertical_store_dogs_small_tile_8._CB312176604_.jpg",
+		            likes: 892,
 					userLike: false,
-								caption: "pigs!",
-								tags: ['elections'],
-								comments: [{
-														id: 0,
-														user: {
-																id: 1,
-																username: "JohnCat",
-																profileImageSmall: "http://bywilma.com/wp-content/uploads/2012/03/Cat-John.jpg"
-														},
-														text: "hahahaha",
-														userRefs: [],
-														tags: []
-												}
-								]
+		            caption: "awesome ball!",
+		            tag: "ball",
+		            comments: [{
+		                        id: 1,
+		                        user: {
+		                            id: 1,
+		                            username: "JohnCat",
+		                            profileImageSmall: "http://bywilma.com/wp-content/uploads/2012/03/Cat-John.jpg"
+		                        },
+		                        text: "Awesome ball!",
+		                        userRefs: [],
+		                        tag: "happy"
+		                    }
+		            ]
 
-						},
+		        }
 			]
 
 
--- index.js
+++ index.js
@@ -4,18 +4,11 @@ var bodyParser  = require('body-parser');
 var app = express();
 
 var posts = require('./Home/Posts.js');
-//var users = require('./Home/Users.js');
 var users = [{
         id: "1",
         username: "test",
-        password: "test"
-    },
-    {
-        id: "2",
-        username: "cat",
-        password: "cat"
+        password: "pwd"
     }
-
     ];
 
 app.set('port', (process.env.PORT || 8200));
@@ -25,7 +18,7 @@ app.use(bodyParser.json());
 app.use(posts);
 
 app.get('/', function(req, res) {
-	res.send("Welcome to InstaappOKOKOK!!");
+	res.send("Welcome to Instaapp!!");
 });
 
 app.post('/login', function(req,res){
@@ -44,7 +37,7 @@ app.post('/login', function(req,res){
     {
         return res.sendStatus(401);
     }
-})
+});
 
 app.post('/signup', function(req,res){
     console.log("test signup");
@@ -65,7 +58,36 @@ app.post('/signup', function(req,res){
     {
         return res.sendStatus(401);
     }
-})
+});
+
+app.get('/users/:username', function(req, res){
+ /*  var items=[];
+    users.forEach(function(item,i){
+    if (item.users.username==req.body.username){
+    items.push(item);
+        }
+      });
+      res.json(items);
+    });*/
+  console.log("Insert name" + req.body.username);
+  for (var i = 0; i < users.length; i++) {
+    if(req.body.username === users[i].username) {
+      res.json(users[i]);
+      return;
+    }
+  }
+  res.json(users[i]);
+});
+
+app.get('/posts/:id', function(req, res) {
+var items=[];
+posts.forEach(function(item,i){
+if (item.user.id==req.params.id){
+items.push(item);
+}
+});
+res.json(items);
+});
 
 
 app.listen(app.get('port'), function() {
